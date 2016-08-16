39f7478f027d8fbe374480aed0178829580540cc
Juan Dausa <juandausa@gmail.com>
2016-08-16 18:01:06 -0300

    Se configura postgreesql como motor de base de datos.

--- src/firstproject/firstproject/settings.py
+++ src/firstproject/firstproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firstdjango',
+        'USER': 'firstdjango',
+        'PASSWORD': 'firstdjango',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
