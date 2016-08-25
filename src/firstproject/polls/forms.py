cfa313abd6f195737dbbe177e619638c7d8a9746
Juan Dausa <juandausa@gmail.com>
2016-08-24 23:00:28 -0300

    Se modifican el estilo general de los títulos, ahora se utiliza h2. Se agrega el form, relacionado a las choices.

--- src/firstproject/polls/forms.py
+++ src/firstproject/polls/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.forms import formset_factory
 
-from .models import Question
+from .models import Question, Choice
 
 # Forms
 
@@ -9,3 +10,17 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('question_text',)
+
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ('choice_text',)
+
+    def __init__(self, *args, **kwargs):
+        super(ChoiceForm, self).__init__(*args, **kwargs)
+
+        for key in self.fields:
+            self.fields[key].required = True
+
+ChoiceFormSet = formset_factory(ChoiceForm, extra=3)
--- src/firstproject/polls/models.py
+++ src/firstproject/polls/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, unique=True)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -33,7 +33,7 @@ class Question(models.Model):
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200, blank=False)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
